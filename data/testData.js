abf3e9415e7c828e3ed53ff1055a6b32bdc2a94d
HannaAhanian <asinaanya@gmail.com>
2021-03-27 16:25:28 -0400

    function

---
+++ data/testData.js
@@ -0,0 +1,23 @@
+const name = {
+ "default": "GroundSquirrel21804"
+}
+ const gender = {
+ "he": 0,
+ "she": 1,
+ "it": 2
+ }
+
+const age = {
+    "default":"0123456789"
+}
+const story ={
+    "overcomingTheMonster": 0,
+    "rebirth": 1,
+    "quest": 2,
+    "journeyAndReturn": 3,
+    "ragsAndRiches": 4,
+    "tragedy": 5,
+    "comedy": 6
+
+}
+module.exports = {name, age, gender, story};
